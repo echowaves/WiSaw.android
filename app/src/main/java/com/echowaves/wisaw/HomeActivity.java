56b5981964673d026c5a9a9420cc237a402ffbc4
dmitryame <dmitry@custommode.com>
2017-10-26 19:30:26 -0400

    wiring things up

--- app/src/main/java/com/echowaves/wisaw/HomeActivity.java
+++ app/src/main/java/com/echowaves/wisaw/HomeActivity.java
@@ -4,7 +4,6 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.echowaves.wisaw.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,11 +12,8 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        ActivityHomeBinding bindings = DataBindingUtil.setContentView(this, R.layout.activity_home);
 
 
-        HomeObject mHome = new HomeObject(0, "Navy blue sandals", 34.50, "Beautiful fitted sandals for work");
-        bindings.setHome(mHome);
 
     }
 }
