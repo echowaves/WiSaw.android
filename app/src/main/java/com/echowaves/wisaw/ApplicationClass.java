4685f3b107175e012f91931a27c723e902ba795b
dmitryame <dmitry@custommode.com>
2018-06-24 11:22:16 -0400

    release: removed notifications

--- app/src/main/java/com/echowaves/wisaw/ApplicationClass.java
+++ app/src/main/java/com/echowaves/wisaw/ApplicationClass.java
@@ -92,8 +92,9 @@ public class ApplicationClass  extends Application  {
     }
 
     public static boolean isPhotoViewed(Integer photoId, Context context) {
-        File file = new File(context.getFilesDir(), "wisaw-viewed-" + photoId.toString());
-        return file.exists();
+        return true;
+//        File file = new File(context.getFilesDir(), "wisaw-viewed-" + photoId.toString());
+//        return file.exists();
     }
 
 
@@ -121,56 +122,57 @@ public class ApplicationClass  extends Application  {
 
 
     public static void updateAppBadge(JSONArray photosJSON, Context context) {
-        int updates = 0;
-        for (int i=0; i < photosJSON.length(); i++) {
-            try {
-                JSONObject photoJSON = photosJSON.getJSONObject(i);
-                Integer photoId = photoJSON.getInt("id");
-                if(!isPhotoViewed(photoId, context)) {
-                    updates++;
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
+        return;
+//        int updates = 0;
+//        for (int i=0; i < photosJSON.length(); i++) {
+//            try {
+//                JSONObject photoJSON = photosJSON.getJSONObject(i);
+//                Integer photoId = photoJSON.getInt("id");
+//                if(!isPhotoViewed(photoId, context)) {
+//                    updates++;
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
 
         // set badge notification
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+//        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        String NOTIFICATION_CHANNEL_ID = "my_channel_id_01";
+//
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "wisaw notifications", NotificationManager.IMPORTANCE_HIGH);
+//
+//            // Configure the notification channel.
+//            notificationChannel.setDescription("default wisaw notification channel");
+//            notificationChannel.enableLights(true);
+//            notificationChannel.setLightColor(Color.RED);
+//            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+//            notificationChannel.enableVibration(true);
+//            notificationManager.createNotificationChannel(notificationChannel);
+//        }
+//
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "wisaw notifications", NotificationManager.IMPORTANCE_HIGH);
-
-            // Configure the notification channel.
-            notificationChannel.setDescription("default wisaw notification channel");
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
-            notificationChannel.enableVibration(true);
-            notificationManager.createNotificationChannel(notificationChannel);
-        }
-
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
-
-        notificationBuilder
-                .setAutoCancel(true)
-                .setDefaults(Notification.DEFAULT_ALL)
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.mipmap.ic_launcher_round)
-//                .setTicker("Hearty365")
-//                .setPriority(Notification.PRIORITY_MAX)
-//                .setContentTitle("Default notification")
-                .setContentText("unseen photos: " + updates)
-//                .setContentInfo("Info")
-                ;
-
-        if(updates > 0) {
-            notificationManager.notify(/*notification id*/1, notificationBuilder.build());
-        } else {
-            notificationManager.cancelAll();
-        }
+//        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+//
+//        notificationBuilder
+//                .setAutoCancel(true)
+//                .setDefaults(Notification.DEFAULT_ALL)
+//                .setWhen(System.currentTimeMillis())
+//                .setSmallIcon(R.mipmap.ic_launcher_round)
+////                .setTicker("Hearty365")
+////                .setPriority(Notification.PRIORITY_MAX)
+////                .setContentTitle("Default notification")
+//                .setContentText("unseen photos: " + updates)
+////                .setContentInfo("Info")
+//                ;
+//
+//        if(updates > 0) {
+//            notificationManager.notify(/*notification id*/1, notificationBuilder.build());
+//        } else {
+//            notificationManager.cancelAll();
+//        }
     }
 
 
